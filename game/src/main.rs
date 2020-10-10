e373315926f7ae00dfb04d46bb40ab8325622288
Fedcomp <aglergen@gmail.com>
2020-10-10 19:56:52 +0300

    Draw player charater in the world

--- game/src/main.rs
+++ game/src/main.rs
@@ -13,5 +13,20 @@ fn main() {
         })
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_default_plugins()
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("assets/player.png").unwrap();
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(SpriteComponents {
+            material: materials.add(texture_handle.into()),
+            ..Default::default()
+        });
 }
