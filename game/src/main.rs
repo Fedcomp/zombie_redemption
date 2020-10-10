4b30fcc60498427844185963de1f44d222d77cf0
Fedcomp <aglergen@gmail.com>
2020-10-10 09:36:15 +0300

    Empty main window

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use bevy::prelude::*;
+
+const GAME_NAME: &str = "Zombie Redemption";
+
+fn main() {
+    App::build()
+        .add_resource(WindowDescriptor {
+            title: GAME_NAME.into(),
+            width: 800,
+            height: 600,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_default_plugins()
+        .run();
+}
